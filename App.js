e4b926f5daea301417d4ee78c332cef3340e6226
Antje Oltmanns <antje.oltmanns@gmail.com>
2021-05-27 00:43:46 +0200

    delete post by id

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
 
-export default function App() {
+const navigator = createStackNavigator (
+  {
+    Index: IndexScreen
+  }, 
+  {
+    initalRouteName: 'Index',
+    defaultNavigationOptions: {
+        title: 'Blogs'
+    }
+  }
+);
+const App = createAppContainer(navigator); 
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider><App /></Provider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
