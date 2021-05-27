f4b4bc16d55bdd0783d0edef892f84d27cd2bf9c
Antje Oltmanns <antje.oltmanns@gmail.com>
2021-05-27 11:32:52 +0200

    Navigate to blog post by id

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
-const { state, addBlogPost } = useContext(Context);
+
+const IndexScreen = ({ navigation }) => {
+   
+const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
     return (
         <View>
@@ -13,11 +15,17 @@ const { state, addBlogPost } = useContext(Context);
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return ( 
-                        <View  style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <Feather style={styles.icon} name="trash-2" />
-                        </View>
+                    return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}> 
+                            <View  style={styles.row}>
+                                <Text style={styles.title}>
+                                {item.title} - {item.id} 
+                                </Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.icon} name="trash-2" />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     );
                 }}
             />
